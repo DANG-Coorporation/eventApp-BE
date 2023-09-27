45d47325f58593f4e3ee8f65f3e5177434c4af98
NaufanRikza <deathriver26@gmail.com>
2023-09-27 14:17:08 +0700

    feature : revision on middleware

--- src/routes/loginRoute.js
+++ src/routes/loginRoute.js
@@ -6,14 +6,12 @@ const router = express.Router();
 
 router.post("/login", Validator.validateLogin, async (req, res) => {
   try {
-    console.log(req.userObj);
     const token = jwt.sign(req.userObj, process.env.JWT_TOKEN);
     res.status(200).json({
       status: 200,
       message: "login success",
       token: token,
     });
-    console.log("succes");
   } catch (e) {
     res.status(500).json({
       status: 500,
